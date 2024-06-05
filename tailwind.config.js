00345727c792d20fd198615ff622c26898e31e38
Hanifur Rahman Sabbir <hanifurrahmansabbir@gmail.com>
2024-06-05 19:59:55 +0600

    Drafted v1 (#1)
    * Homepage changes
    
    * changes
    
    * Create CNAME
    
    * Update CNAME
    
    * Update CNAME
    
    * Delete CNAME
    
    * Create CNAME
    
    * Delete CNAME
    
    * Publish changes
    
    * changes
    
    * Testing styles
    
    * Create CNAME
    
    * fixed favicon
    
    * update
    
    * update
    
    * changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
+  content: ['*.html', '_site/*.html','_site/**/*.html', '_pages/**/*.md', '_pages/**/*.html', '_posts/**/*.md', '_layouts/**/*.html', '_includes/**/*.html'],
   theme: {
     extend: {
       colors: {
